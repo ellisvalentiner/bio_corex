0d11eb1e2895c2b051fc1b4fe5ce9e242b7d2694
Ellis Valentiner <ellisvalentiner@gmail.com>
2017-08-01 12:17:11 -0400

    Frankly, my dear [Travis], I don't give a damn [about Python 3.7-dev]

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='corex',
-      version='1.0',
-      description='Python Implementation of Correlation Explanation',
-      author='Greg Ver Steeg',
-      author_email='gregv@isi.edu',
-      url='https://github.com/gregversteeg/bio_corex',
-      install_requires=[
-          'numpy>=1.8.0',
-          'scipy>=0.13.0'
-      ])
+setup(
+    name='corex',
+    version='1.0',
+    description='Python Implementation of Correlation Explanation',
+    author='Greg Ver Steeg',
+    author_email='gregv@isi.edu',
+    url='https://github.com/gregversteeg/bio_corex',
+    packages=['corex', ],
+    modules=['corex', ],
+    install_requires=[
+        'numpy>=1.8.0',
+        'scipy>=0.13.0'
+    ])
